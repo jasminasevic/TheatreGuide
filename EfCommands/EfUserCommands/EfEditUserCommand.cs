0c5c048bb5c717d5f03c3bdccb0e9f688975cd2b
jasminasevic <jasminasevic@yahoo.com>
2021-03-07 19:38:38 +0100

    Send email when business user is approved or declined

--- EfCommands/EfUserCommands/EfEditUserCommand.cs
+++ EfCommands/EfUserCommands/EfEditUserCommand.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Commands.UserCommands;
+using Application.DTO.EmailDto;
 using Application.DTO.UserDto;
+using Application.Email;
 using Application.Exceptions;
 using Application.Interfaces;
 using Application.Validators.UserValidators;
@@ -15,10 +17,12 @@ namespace EfCommands.EfUserCommands
     public class EfEditUserCommand : EfBaseCommand, IEditUserCommand
     {
         protected readonly UserValidator _validator;
-        public EfEditUserCommand(EfContext context, UserValidator validator) 
+        private readonly IEmailSender _sender;
+        public EfEditUserCommand(EfContext context, UserValidator validator, IEmailSender sender)
             : base(context)
         {
             _validator = validator;
+            _sender = sender;
         }
 
         public int Id => 76;
@@ -32,6 +36,7 @@ namespace EfCommands.EfUserCommands
             _validator.ValidateAndThrow(request);
 
             var user = Context.Users.Find(request.Id);
+            var previousUserStatus = user.Status.ToString();
 
             if (user.Email != request.Email
                && Context.Users.Any(u => u.Email == request.Email))
@@ -50,6 +55,31 @@ namespace EfCommands.EfUserCommands
             user.ModifiedAt = DateTime.Now;
 
             Context.SaveChanges();
+
+            if(previousUserStatus != request.Status && request.RoleId == 2) 
+            { 
+                if(request.Status == "Approved") 
+                {
+                    _sender.Send(new SendEmailDto
+                    {
+                        Content = "<h1>You application has been approved!</h1>" +
+                        "<h2> Login to your account to proceed.</h2>",
+                        SendTo = request.Email,
+                        Subject = "Application Approved"
+                    });
+                }
+                if (request.Status == "Declined")
+                {
+                    _sender.Send(new SendEmailDto
+                    {
+                        Content = "<h1>You application has been declined!</h1>" +
+                        "<h2> We're sorry to let you know that your application to Theatre Guide has been rejected.</h2>",
+                        SendTo = request.Email,
+                        Subject = "Application Rejected"
+                    });
+                }
+
+            }
         }
     }
 }
